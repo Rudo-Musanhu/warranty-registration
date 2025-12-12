12978ecbaa978a3ffa33841c8c5a5ae5630a79db
Rudo <rudomusanhu@gmail.com>
2025-12-12 21:00:17 +0200

    settings

--- warranty_backend/settings.py
+++ warranty_backend/settings.py
@@ -61,7 +61,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://localhost:3001",
-    "http://127.0.0.1:3001", 
+    "http://127.0.0.1:3001",
+    "https://assetwarranty-a4x8ralbc-rudo-musanhus-projects.vercel.app" 
 ]
 
 CORS_ALLOW_CREDENTIALS = True
